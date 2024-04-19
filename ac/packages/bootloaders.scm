fec4df873e3a5fa0c08f19a47d2ee55f3f0f911c
Jiwei Yang <yangjiwei@protonmail.com>
2024-04-20 00:14:49 +0200

    ac: grub-efi-luks2: Fix builds. Follow definitions from rakino/Rosenthal. * ac/packages/stb.scm (grub-runtime-memregion-alloc-patch,grub-luks2-argon2-support-patch,grub-install-luks2-fix-patch): New variables.

--- ac/packages/bootloaders.scm
+++ ac/packages/bootloaders.scm
@@ -1,6 +1,5 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;; Copyright © 2023 Hilton Chain <hako@ultrarare.space>
-;;; Copyright © 2023 Jiwei YANG <yangjiwei@protonmail.com>
 
 (define-module (ac packages bootloaders)
   #:use-module (guix download)
@@ -14,6 +13,36 @@
 ;; Patches obtained from:
 ;; <https://leo3418.github.io/collections/gentoo-config-luks2-grub-systemd/packages.html>
 
+(define grub-runtime-memregion-alloc-patch
+  (origin
+    (method url-fetch)
+    (uri (string-append "https://leo3418.github.io/"
+                        "res/collections/gentoo-config-luks2-grub-systemd/"
+                        "4500-grub-2.06-runtime-memregion-alloc.patch"))
+    (sha256
+     (base32
+      "1ynlyaabdlw7g4pqr4sc7jdqn9riwms637cqhlqnyi8jp6cs4a6x"))))
+
+(define grub-luks2-argon2-support-patch
+  (origin
+    (method url-fetch)
+    (uri (string-append "https://leo3418.github.io/"
+                        "res/collections/gentoo-config-luks2-grub-systemd/"
+                        "5000-grub-2.06-luks2-argon2-v4.patch"))
+    (sha256
+     (base32
+      "0jfyl2j9bh0225iqq36gy55p2l96zhhhsk5r53aqmlivvxpjlf7g"))))
+
+(define grub-install-luks2-fix-patch
+  (origin
+    (method url-fetch)
+    (uri (string-append "https://leo3418.github.io/"
+                        "res/collections/gentoo-config-luks2-grub-systemd/"
+                        "9500-grub-AUR-improved-luks2.patch"))
+    (sha256
+     (base32
+      "0hn0nkp5cfsyvvyfj3vbw0z4c2swdi6gvxradl401lqvwsvq60p4"))))
+
 (define-public grub-efi-luks2
   (let ((base grub-efi))
     (package
@@ -25,9 +54,9 @@
            (inherit base)
            (patches
             (append (origin-patches base)
-                    (list (local-file "patches/4500-grub-2.06-runtime-memregion-alloc.patch")
-                          (local-file "patches/5000-grub-2.06-luks2-argon2-v4.patch")
-                          (local-file "patches/9500-grub-AUR-improved-luks2.patch")))))))
+                    (list grub-runtime-memregion-alloc-patch
+                          grub-luks2-argon2-support-patch
+                          grub-install-luks2-fix-patch))))))
       (arguments
        (substitute-keyword-arguments (package-arguments base)
          ((#:configure-flags flags ''())
